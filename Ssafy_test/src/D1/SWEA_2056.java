27e245b6043dbdce5166a15ef56121bec49ddfac
sunJ <sspure1214@gmail.com>
2024-10-23 18:36:28 +0900

    [d1] 2056 string이 주어졌을때, 비교를 위해서 년/월/일 단위로 잘라야 하는데, 구분자가 없으므로 나누기를 이용해서 잘라냈다.(1000원짜리...구하듯이)

---
+++ Ssafy_test/src/D1/SWEA_2056.java
@@ -0,0 +1,70 @@
+package D1;
+
+import java.io.*;
+
+/*
+ * 풀이 전략
+ * * 유효성 검사를 단계별로 나눠서 진행한다.
+ * 1. 년도에 대한 유효성 검사 : 0000 ~ 9999
+ * 2. 월에 대한 유효성 검사 : 01 ~ 12
+ * 3. 일에 대한 유효성 검사 : 1,3,5,7,8,10,12 = 31
+ * 4,6,8,10 = 31
+ * 2 = 28
+ * 
+ * 4,2,2 단위로 잘라서 숫자로 바꿔서 사용한다.
+ */
+
+
+public class SWEA_2056 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i<=num;i++) {
+			String str = br.readLine();
+			int n = Integer.parseInt(str);
+			int year = n /10000;
+			int month = (n % 10000) / 100;
+			int day = (n % 10000) % 100; 
+			
+//			System.out.println("year = "+ year);
+//			System.out.println("month = "+ month);
+//			System.out.println("day = "+ day);
+			
+			boolean t = false;
+			
+			if(year > 0) {
+				if(1<=month && month<=12) {
+					if(month == 2) {
+						if(1 <= day&&28 >= day) {
+							t = true;
+						}
+					}else if(month == 4||month == 6||month == 8||month == 10) {
+						if(1 <= day&&30 >= day) {
+							t = true;
+						}
+					}else {
+						if(1 <= day&&31 >= day) {
+							t = true;
+						}
+					}
+				}
+			}
+			if(t) {
+				bw.write("#" + i + " ");
+				bw.write(str.substring(0,4) + "/");
+				bw.write(str.substring(4,6) + "/");
+				bw.write(str.substring(6,8) + "\n");
+			}else {
+				bw.write("#" + i + " ");
+				bw.write(-1 + "\n");
+			}
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
